2b191b1fd683ae0a35d4afaa19a5349bd4404622
Neil Hoskins <neil.hoskins2@sa.gov.au>
2022-05-16 03:46:01 +0930

    simple tree

--- tests/p55_btree.pl
+++ tests/p55_btree.pl
@@ -3,9 +3,7 @@ btree(N, Tree) :-
     n_nodes(Tree, N),
     btree_(Tree, _).
 
-btree_(l(x), c(zero)).
-btree_(tl(x,l(x)), c(c(zero))).
-btree_(tr(x,l(x)), c(c(zero))).
+btree_(nil, zero).
 btree_(t(x,L,R), D) :-
     btree_(L, D1),
     btree_(R, D2),
@@ -17,11 +15,7 @@ valid_depth(c(c(zero)), zero, c(zero)).
 valid_depth(c(Z), c(A), c(B)) :-
     valid_depth(Z, A, B).
 
-n_nodes(l(_), c(zero)).
-n_nodes(tl(_,T), c(N)) :-
-    n_nodes(T, N).
-n_nodes(tr(_,T), c(N)) :-
-    n_nodes(T, N).
+n_nodes(nil, zero).
 n_nodes(t(_,L,R), c(N)) :-
     add(Nl,Nr,N),
     n_nodes(L,Nl),
