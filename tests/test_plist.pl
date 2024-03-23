33ef72f71814a8ada70f14b77af3409092824ca6
neil hoskins <neil.hosk@pm.me>
2024-03-23 17:22:46 +1030

    fixing base64 code

--- tests/test_plist.pl
+++ tests/test_plist.pl
@@ -4,20 +4,20 @@
 
 test(plist_pcompare_empty_lists, A) :-
     pcompare([], [], C),
-    pif(C = '=', A = true, A = false).
+    pif(eq(C, '='), unify(A, true), unify(A, false)).
 
 test(plist_pcompare_empty_one_element, A) :-
     pcompare([], [a], C),
-    pif(C = '<', A = true, A = false).
+    pif(eq(C, '<'), unify(A, true), unify(A, false)).
 
 test(plist_pcompare_equal_elements, A) :-
     pcompare([a], [a], C),
-    pif(C = '=', A = true, A = false).
+    pif(eq(C, '='), unify(A, true), unify(A, false)).
 
 test(plist_pcompare_lt_elements, A) :-
     pcompare([a], [b], C),
-    pif(C = '<', A = true, A = false).
+    pif(eq(C, '<'), unify(A, true), unify(A, false)).
 
 test(plist_pcompare_gt_elements, A) :-
     pcompare([t], [b], C),
-    pif(C = '>', A = true, A = false).
+    pif(eq(C, '>'), unify(A, true), unify(A, false)).
